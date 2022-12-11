1fcf73c250c04266e8125bb4b7faa79e7baf8b14
Natali <mishina_nv00@mail.ru>
2022-12-11 20:36:51 +0300

    Сделали дз4

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3,5 -> 243 (3⁵)
+//2,4 -> 16
+
+
+double Degree(int A, int B)
+{
+    double result = Math.Pow(A, B);
+    return result;
+}
+
+int EnterData(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int A = EnterData("Введите 1 число: ");
+int B = EnterData("Введите 2 число: ");
+double result = Degree(A, B);
+Console.WriteLine("Число " + A + " в степени числа " + B + " равен " + result);
---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Sum(int s)
+{
+    int result = 0;
+    while (s > 0)
+    {
+        result = result + s % 10;
+        s = s / 10;
+    }
+    return result;
+}
+
+int EnterData(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int s = EnterData("Введите число: ");
+int result = Sum(s);
+Console.WriteLine("Сумма цфр в числе " + s + " равна " + result);
